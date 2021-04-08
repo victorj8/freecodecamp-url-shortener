0928ec843f58a03cd1db375ad3f8e1d685cca205
Victor Camacho <victor.camacho@block.one>
2021-04-08 13:36:17 -0400

    adding main functionality

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+var mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: Number
+});
+
+let Url = mongoose.model('Url', urlSchema);
+
+const createAndSaveUrl = (url, done) => {
+  Url.count({}, function(err, count){
+    let urlObj = Url({original_url:  url, short_url: count});
+    urlObj.save(function(err, data){
+      if (err) return console.error(err);
+      done(null , data);
+    });
+  });
+};
+
+const findShortUrl = (url, done) => {
+  Url.find({short_url: parseInt(url)}, function(err, val){
+    if (err) return console.error(err);
+    done(null , val);
+  });
+};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +52,29 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl/:url', function(req, res) {
+  findShortUrl(req.params.url,(err, data)=>{
+    if(err) { res.json({"error": 'invalid url'}); return; }
+    res.redirect(data[0].original_url);
+  });
+});
+
+app.post('/api/shorturl/new',function(req, res, next) {
+  let urlvar = new URL(req.body.url);
+
+  dns.lookup(urlvar.hostname, (err, address, family)=>{
+    if (err) {
+      res.json({"error": 'invalid url'});
+    } else {
+      next();
+    }
+  });
+  }, function(req, res) {
+    createAndSaveUrl(req.body.url, (err, data) => {
+      res.send({original_url : data.original_url, short_url : data.short_url});
+    });
+  });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
